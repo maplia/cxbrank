292bb39860e01c54816bbc0d36a24cd016d6c838
Tsuyoshi Shimabukuro <sie@maplia.jp>
2015-02-24 23:25:20 +0900

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cxbrank::Application.config.secret_key_base = 'b65fbdddc29067b519ac1d1c6798b691148ec1a590473fd3b0177e0c2d5160cf6130479f3524728fb254b0d4c97ec0dfa5a43527588f67cc2999d022540a4414'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cxbrank::Application.config.session_store :cookie_store, key: '_cxbrank_session'
