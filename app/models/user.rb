ac5cfad9f62e2b4df3472c4098bc8419f9d6a89a
Tsuyoshi Shimabukuro <sie@maplia.jp>
2015-03-23 18:07:19 +0900

    2015/03/23_4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
     length: {is: 8, message: 'CROSS×BEATS IDは8文字で入力してください'},
     format: {with: /\A\d+\z/, message: 'CROSS×BEATS IDは数字のみで入力してください'}, allow_blank: true
 
-  before_save :crypt_password
+  before_save :crypt_password!
 
   scope :current, ->(session) {find(session[:user_id].to_i)}
   scope :find_by_params, ->(params) {find(params[:id].to_i)}
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
     return self.find_by(id: user_id.to_i, password: Digest::SHA1.hexdigest(password))
   end
 
-  def crypt_password
+  def crypt_password!
     if new_record? or changed.include?('password')
       self.password = Digest::SHA1.hexdigest(password)
       self.password_confirmation = Digest::SHA1.hexdigest(password_confirmation)
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,8 +5,42 @@ class UserTest < ActiveSupport::TestCase
     assert_nil User.authenticate('00001', '')
     assert_nil User.authenticate('00001', Digest::SHA1.hexdigest('testuser'))
 
-    result = User.authenticate('00001', 'testuser')
-    assert_instance_of User, result
-    assert_equal result.username, users(:testuser1).username
+    user = User.authenticate('00001', 'testuser')
+    assert_instance_of User, user
+    assert_equal user.username, users(:testuser1).username
+  end
+
+  test "crypt_password!" do
+    user = User.new({
+      username: 'testuser1', password: 'testuser1', password_confirmation: 'testuser1'
+    })
+    user.crypt_password!
+    assert_equal Digest::SHA1.hexdigest('testuser1'), user.password
+
+    user = User.create!({
+      username: 'testuser1', password: 'testuser1', password_confirmation: 'testuser1'
+    })
+    old_password = user.password.dup
+    user.update({password: user.password, password_confirmation: user.password})
+    user.crypt_password!
+    assert_equal old_password, user.password
+    assert_equal Digest::SHA1.hexdigest('testuser1'), user.password
+
+    user = User.create!({
+      username: 'testuser1', password: 'testuser1', password_confirmation: 'testuser1'
+    })
+    old_password = user.password.dup
+    user.update({password: 'testuser2', password_confirmation: 'testuser2'})
+    user.crypt_password!
+    assert_not_equal old_password, user.password
+    assert_equal Digest::SHA1.hexdigest('testuser2'), user.password
+  end
+
+  test "save!" do
+    user = User.create!({
+      username: 'testuser1', password: 'testuser1', password_confirmation: 'testuser1'
+    })
+    assert_not_equal 'testuser1', user.password
+    assert_equal Digest::SHA1.hexdigest('testuser1'), user.password
   end
 end
